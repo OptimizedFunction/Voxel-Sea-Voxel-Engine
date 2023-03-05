67409b87afbf7e69df9320d0ddea235182f4081e
OptimizedFunction <95468660+OptimizedFunction@users.noreply.github.com>
2023-03-06 01:46:55 +0530

    Revert "Merge pull request #1 from OptimizedFunction/Dev"
    This reverts commit 7ec3dc2bddcc0676da9ff9708b5bb2c743c843eb, reversing
    changes made to 3c1cef6e054afb7bcd21c8834d4bdef130cd66c5.

--- VoxelSea 1.0.0/ServerScriptService/ServerInitialiser.server.lua
+++
@@ -1,3 +0,0 @@
-local modules = require(game:GetService('ReplicatedStorage')["VoxelSea 2.0"].Modules.ModuleIndex)
-local replicator = require(modules.ReplicatorAndUpdateLogger)
-replicator.InitialiseServer()
--- VoxelSea 1.0.0/StarterGui/Misc/unstuck/unstuck.client.lua
+++
@@ -1,6 +0,0 @@
-script.Parent.MouseButton1Click:Connect(function()
-	
-	local plr = game:GetService('Players').LocalPlayer
-	local char = plr.Character
-	if char then char:MoveTo(Vector3.new(char:GetPrimaryPartCFrame().Position.X, 400, char:GetPrimaryPartCFrame().Position.Z)) end
-end)
